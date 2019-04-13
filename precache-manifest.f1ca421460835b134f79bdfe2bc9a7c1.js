92eedd104681d82b8133a30406e2e25637a7b032
artoniaz <artoni.az@gmail.com>
2019-04-13 21:37:15 +0200

    Updates

---
+++ precache-manifest.f1ca421460835b134f79bdfe2bc9a7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4f64604b70c57fd94876e23fe34d82d",
+    "url": "/tic-tac-toe/static/media/backgroundImg.b4f64604.jpg"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "c9526927c176b890132d",
+    "url": "/tic-tac-toe/static/js/main.83f55b4f.chunk.js"
+  },
+  {
+    "revision": "0176bb5f5451815120b5",
+    "url": "/tic-tac-toe/static/js/2.e058b2c1.chunk.js"
+  },
+  {
+    "revision": "c9526927c176b890132d",
+    "url": "/tic-tac-toe/static/css/main.34fb7ff2.chunk.css"
+  },
+  {
+    "revision": "d605a5af28dc93365f26caedbaf1f9c9",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
